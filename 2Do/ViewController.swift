7b5ac3f446d91bd4217dec62e22b048e349a062b
aaranans11 <aaranans11@gmail.com>
2022-10-02 18:08:33 -0400

    Initial Commit

---
+++ 2Do/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2Do
+//
+//  Created by Aaranan Sathiendran on 2022-10-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
